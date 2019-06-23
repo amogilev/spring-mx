01b0e835c3e83ea68ab5ff659e63423f45b69eb3
Andrey Mogilev <amogilev@gmail.com>
2019-06-23 17:38:17 +0700

    add action "Show active sessions"

---
+++ src/main/java/com/gilecode/xmx/smx/sessions/ShowSessionsListAction.java
@@ -0,0 +1,45 @@
+package com.gilecode.xmx.smx.sessions;
+
+import com.intellij.ide.browsers.BrowserLauncher;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.popup.*;
+import com.intellij.openapi.ui.popup.util.BaseListPopupStep;
+import com.intellij.ui.components.JBLabel;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+public class ShowSessionsListAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        List<XmxSession> sessions = XmxSessionWatcherService.getInstance().getActiveSessions();
+        if (sessions.isEmpty()) {
+            // null stands for "no sessions"
+            sessions.add(null);
+        }
+        ListPopupStep<XmxSession> step = new BaseListPopupStep<XmxSession>("Active SpringMX sessions", sessions) {
+            @Nullable
+            @Override
+            public PopupStep onChosen(XmxSession session, boolean finalChoice) {
+                if (session != null) {
+                    BrowserLauncher.getInstance().browse("http://localhost:" + session.getPort() + "/smx/", null);
+                }
+                return super.onChosen(session, finalChoice);
+            }
+
+            @NotNull
+            @Override
+            public String getTextFor(XmxSession session) {
+                return session == null ? "<no active sessions>" : session.toString();
+            }
+        };
+        ListPopup listPopup = JBPopupFactory.getInstance().createListPopup(step);
+        listPopup.showInFocusCenter();
+    }
+}
--- src/main/java/com/gilecode/xmx/smx/sessions/XmxSession.java
+++ src/main/java/com/gilecode/xmx/smx/sessions/XmxSession.java
@@ -3,15 +3,22 @@ package com.gilecode.xmx.smx.sessions;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class XmxSession {
     private final Integer port;
     private final ExecutionEnvironment env;
     private final ProcessHandler handler;
+    private final LocalDateTime started;
 
     public XmxSession(Integer port, ExecutionEnvironment env, ProcessHandler handler) {
         this.port = port;
         this.env = env;
         this.handler = handler;
+        this.started = LocalDateTime.now();
     }
 
     public Integer getPort() {
@@ -25,4 +32,18 @@ public class XmxSession {
     public ProcessHandler getHandler() {
         return handler;
     }
+
+    @Override
+    public String toString() {
+        return env.toString() + "; started " + getStartedFormatted() + ", port " + port;
+    }
+
+    private String getStartedFormatted() {
+        if (started.toLocalDate().equals(LocalDate.now())) {
+            return DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).format(started);
+        } else {
+            return DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).format(started);
+        }
+    }
+
 }
